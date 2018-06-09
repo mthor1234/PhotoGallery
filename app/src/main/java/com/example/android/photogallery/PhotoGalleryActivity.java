ec6e9cfe33b6440f20903a51037fe998086cdddc
MJ Thornton <you@example.com>
2018-06-09 10:21:14 -0700

    Commiting the first batch of files...
    The Big Nerd Ranch Guide Android Programming 2nd Edition
    Covers up to Chapter 26 Pg 484

---
+++ app/src/main/java/com/example/android/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.photogallery;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    public static Intent newIntent(Context context){
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
+
+    @Override
+    public Fragment createFragment(){
+        return PhotoGalleryFragment.newInstance();
+    }
+}
